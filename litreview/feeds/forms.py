15ae0a92e620c533c0e4b8c2b17f208a1a3a00b5
Aurore DUCAP <aurore.ducap@gmail.com>
2022-08-05 12:12:18 +0400

    Home page revised and create ticket page and form

--- litreview/feeds/forms.py
+++ litreview/feeds/forms.py
@@ -5,9 +5,18 @@ from feeds import models
 class TicketForm(forms.ModelForm):
     edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
+    description = forms.CharField(max_length=2048, widget=forms.Textarea(attrs={"class": "form-control"}))
+
     class Meta:
         model = models.Ticket
         fields = ["title", "description", "image"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
+        self.fields["title"].widget.attrs.update(
+            {"class": "form-control", }
+            )
 
 
 class ReviewForm(forms.ModelForm):
@@ -16,7 +25,3 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
         fields = ["headline", "rating", "body"]
-
-
-class DeletePost(forms.Form):
-    delete_post = forms.BooleanField(widget=forms.HiddenInput, initial=True)
